3b4c3f025fe60cff65a8761c7d3ceac1b1c41675
Lucas Mauricio <lucasmauriciofc@gmail.com>
2024-02-24 11:22:01 -0300

    fix: comandos de deploy

--- src/server.ts
+++ src/server.ts
@@ -9,4 +9,9 @@ const port = process.env.PORT || 3333
 app.use(json())
 app.use(routes)
 
-app.listen(port, () => console.log(`🚀 App is running at port ${port}!`))
+// app.listen(port, () => console.log(`🚀 App is running at port ${port}!`))
+
+app.listen({
+  host: '0.0.0.0',
+  port: process.env.PORT ? Number(process.env.PORT) : 3333,
+})
